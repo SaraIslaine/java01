b388c04a814aa22005c2168bf58bc4f2144e10a0
Sara <saraislaine01@gmail.com>
2026-05-29 16:45:00 -0300

    atual

---
+++ src/dolar/Ativ.java
@@ -0,0 +1,28 @@
+package dolar;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import util.Calculator;
+
+public class Ativ {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("What is the dollar price? ");
+		double dollar = sc.nextDouble();
+
+		System.out.print("What is the dollar price? ");
+		double many = sc.nextDouble();
+
+		
+
+		System.out.printf("circumferencia %.2f%n");
+
+		sc.close();
+	}
+
+}
